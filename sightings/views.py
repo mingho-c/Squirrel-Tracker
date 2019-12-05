eeeedb78db9ee937ba2c96d295921656a872c4bb
Ming Ho Cheung <mc4793@columbia.edu>
2019-12-05 21:44:31 +0000

    Rename function 'all_squirrels' to 'all' in views

--- sightings/views.py
+++ sightings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from sightings.models import Squirrel
 
-def all_squirrels(request):
+def all(request):
     squirrels = Squirrel.objects.all()
     context = {
             's': squirrels,
