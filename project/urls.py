9ad1da63f29e10697fa9e43ac768372b53d81c73
Ming Ho Cheung <mc4793@columbia.edu>
2019-12-05 11:20:00 +0000

    Rename 'squirrels' to 'sightings'

--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from squirrels import views
+from sightings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sightings/', include('squirrels.urls')),
+    path('sightings/', include('sightings.urls')),
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from squirrels.models import Squirrel
+from sightings.models import Squirrel
 
 def all_squirrels(request):
     squirrels = Squirrel.objects.all()
     context = {
             's': squirrels,
             }
-    return render(request, 'squirrels/all.html', context)
+    return render(request, 'sightings/all.html', context)
