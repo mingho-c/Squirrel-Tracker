2ec1c93d9aef619e549158089f4807723bd1500e
Ming Ho Cheung <mc4793@columbia.edu>
2019-12-06 01:48:10 +0000

    Create add function, template and link to urls

--- sightings/views.py
+++ sightings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from sightings.models import Squirrel
+from sightings.forms import SquirrelForm
 
 def all(request):
     squirrels = Squirrel.objects.all()
@@ -8,3 +9,17 @@ def all(request):
             's': squirrels,
             }
     return render(request, 'sightings/all.html', context)
+
+
+def add(request):
+    if request.method == 'POST':
+        form = SquirrelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/sightings')
+    else:
+        form = SquirrelForm()
+    context = {
+            'f': form,
+            }
+    return render(request, 'sightings/add.html', context)
