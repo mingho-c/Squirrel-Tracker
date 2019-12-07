46d19d898fbe023c4def5853d4ab8df0586bacdb
kainan-li <56053041+kainan-li@users.noreply.github.com>
2019-12-07 13:43:28 -0500

    Update the clarity of Forms.py

--- sightings/forms.py
+++ sightings/forms.py
@@ -7,10 +7,10 @@ class SquirrelForm(ModelForm):
     class Meta:
         model = Squirrel
         fields =['x', 'y', 'unique_squirrel_id', 'shift', 'date', 'age',
-                  'primary_fur_color', 'location', 'specific_location', 'running',
-                  'chasing', 'climbing', 'eating', 'foraging', 'other_activities',
-                  'kuks', 'quaas', 'moans', 'tail_flags', 'tail_twitches',
-                  'approaches', 'indifferent', 'runs_from'] 
+                 'primary_fur_color', 'location', 'specific_location', 'running',
+                 'chasing', 'climbing', 'eating', 'foraging', 'other_activities',
+                 'kuks', 'quaas', 'moans', 'tail_flags', 'tail_twitches',
+                 'approaches', 'indifferent', 'runs_from'] 
         
         labels = {
                 'x': _('latitude'),
